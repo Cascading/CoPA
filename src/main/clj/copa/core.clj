921d299c54e42cc97feec73f4134b5653251fedf
Paco Nathan <ceteri@gmail.com>
2013-01-12 17:22:31 -0800

    refactor park subsquery

--- src/main/clj/copa/core.clj
+++ src/main/clj/copa/core.clj
@@ -11,17 +11,36 @@
 
 
 (defn parse-gis [line]
+  "use parse-csv to parse complex CSV format in GIS export"
   (first (csv/parse-csv line))
  )
 
 
+(defn etl-gis [gis trap]
+  "generator to parse fields from the GIS source tap"
+  (<- [?blurb ?misc ?geo ?kind]
+      (gis ?line)
+      (parse-gis ?line :> ?blurb, ?misc, ?geo, ?kind)
+      (:distinct false)
+      (:trap (hfs-textline trap))
+   )
+ )
+
+
+(defn get-parks [src]
+  "filter/parse the park data"
+  (<- [?blurb, ?misc, ?geo, ?kind]
+      (src ?blurb, ?misc, ?geo, ?kind)
+      (re-matches #"\s+Community Type\:\s+Park.*" ?misc)
+   )
+ )
+
+
 (defn -main [in out trap & args]
-  (?<- (hfs-delimited out)
-       [?blurb ?misc ?geo ?kind]
-       ((hfs-delimited in) ?line)
-       (parse-gis ?line :> ?blurb, ?misc, ?geo, ?kind)
-       (:trap (hfs-textline trap))
-       (:distinct false)
-       (re-matches #"\s+Community Type\:\s+Park.*" ?misc)
+  (let [gis (hfs-delimited in)
+        src (etl-gis gis trap)]
+    (?- (hfs-delimited out)
+        (get-parks src)
+     )
    )
  )
